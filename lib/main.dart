cf46bd029f8574d5a3037b892641678cc8035d40
Edria Taji <116660805+edris-taji@users.noreply.github.com>
2023-06-14 10:30:42 +0430

    last changes

--- lib/main.dart
+++ lib/main.dart
@@ -4,32 +4,17 @@ void main() => runApp(BMICalculator());
 
 class BMICalculator extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: InputPage(),
-    );
-  }
-}
-
+  Widget build(BuildContext context) {return MaterialApp(home: InputPage());}}
 class InputPage extends StatefulWidget {
   @override
   State<InputPage> createState() => _InputPageState();
 }
-
 class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
+    return Scaffold(   appBar: AppBar(    title: Text('BMI calculator'))
+      body: Center(child: Text('The Text') )
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
-      ),
-    );
+        onPressed: () {}child: Icon(Icons.add)));
   }
 }
